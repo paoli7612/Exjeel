8b24f5d6f452319fa20bd2a21121fccd34f68474
Tommaso Paoli <tomaoli7612@gmail.com>
2021-12-03 10:27:49 +0100

    non INFO use WARN level Logging

--- src/graphic/Table.java
+++ src/graphic/Table.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableColumn;
 
+import core.Pos;
+
 import java.awt.*;
 
 public class Table extends JTable {
@@ -19,17 +21,27 @@ public class Table extends JTable {
 		for (int i=0; i<width; i++) {			
 			this.getColumnModel().getColumn(i).setCellRenderer(rightRenderer);
 		}
-		
+
+		// imposta NUMERI
 		for (int i=1; i<this.getRowCount(); i++) {
 			this.setValueAt(i, i, 0);
 		}
-
+		
+		// imposta LETTERE
 		for (int i=1; i<this.getColumnCount(); i++) {
 			this.setValueAt(Character.toString((char)64+i), 0, i);
 		}
 		
 		this.setPreferredSize(new Dimension(width*75, height*20));
 		this.setCellSelectionEnabled(showVerticalLines);
+	}
+	
+	public void write(Integer x, Integer y, float value) {
+		
+	}
+	
+	public void write(Pos pos, float value) {
+		write(pos.getX(), pos.getY(), value);
 	}
 	
 	@Override
