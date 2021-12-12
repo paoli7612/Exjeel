77b271b2370989cf8b44176ba85a543ec123b1ad
Tommaso Paoli <tomaoli7612@gmail.com>
2021-12-12 22:19:45 +0100

    first test

--- src/App/App.java
+++ src/App/App.java
@@ -3,8 +3,9 @@ package App;
  * Classe principale del progetto. La applicazione verra fatta partire tramite questa classe
  */
 
-import java.util.ArrayList;
-import java.util.List;
+import core.File;
+import core.Pos;
+import graphic.Window;
 import logging.*;
 
 /**s
@@ -14,22 +15,61 @@ import logging.*;
  */
 public class App {
 	
-	public final static int RIGHE = 10;
+	public final static int RIGHE = 5;
 	public final static int COLONNE = 10;
+	
+	public static File file;
+	public static Window finestra;
+	
+	public App(String[] args) {
+		if (args.length == 0) {
+			new Info("Nessun argomento - nuovo file");
+			file = new File();
+		} else {
+			new Critical("Passasto argomenti");
+			file = new File();
+		}
+	}
 	
 	public static void main(String[] args) {
 		Log.start(Log.INFO, true, "log.log");
-		new Warn("START APP");
-		
-		
-		
-		if (args.length == 0) {
-			
-		} else {
-			new Critical("Passasto argomenti");
-		}
-		
+		new Warn("START APP");	
+				
+		new App(args);
 		
 		new Warn("END APP");
+	}
+
+	public static void nuovo_foglio() {
+		 new Warn("Nuovo foglio");
+	}
+
+	public static void salva() {
+		new Warn("Salva");
+	}
+
+	public static void chiudi() {
+		new Warn("Chiudi");
+	}
+
+	public static void carica(String path) {
+		new Warn("Carica: " + path);
+	}
+
+	public static void elimina_foglio(int index) {
+		new Warn("Elimina foglio : " + index);
+	}
+
+	public void usaFoglio(int i) {
+		
+	}
+
+	public void scrivi(String pos, Float value) {
+		file.scrivi(new Pos(pos), value);
+	}
+
+	public void scrivi(String pos, Integer i) {
+		scrivi(pos, i.floatValue());
+		
 	}
 }
