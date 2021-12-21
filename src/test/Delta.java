bc05534685ad724d5c577153bda0fa17e50717aa
Tommaso Paoli <tomaoli7612@gmail.com>
2021-12-21 12:25:42 +0100

    somma e sottrazione cambio valore

--- src/test/Delta.java
+++ src/test/Delta.java
@@ -2,23 +2,26 @@ package test;
 
 import App.App;
 import logging.Log;
-import utils.Parse;
 
 public class Delta {
 
 	public static void main(String[] args) {
-		Log.start(Log.INFO, true);
+		Log.start(Log.NULL, true);
 		App app = new App(false);
 		
 		app.scrivi("A1", 10);
 		app.scrivi("B1", 15);
+			
+		app.scrivi("A3", "Somma");
+		app.scrivi("B3", "=A1+B1");
 		
-		app.scrivi("C2", "=A1+B1");
+		app.scrivi("A4", "Sottr");
+		app.scrivi("B4", "=A1-B1");
 		
 		app.print();
 		
-		app.scrivi("B1", 0);
-					
+		app.scrivi("B1", 80);
+							
 		app.print();
 
 	}
